61633360f7ad60c9a0bcfde4220a2e24e70d1281
Pragati Pathak <pragati.pthk@gmail.com>
2020-06-07 00:16:22 +0530

    Initial Commit Infra Ioc project

---
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,19 @@
+﻿using MicroRabbit.Infra.Bus;
+using Microservice.Domain.Core.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        //we can use this across any microservice, mvc, presentation service or console app to register dependency container
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMqBus>()
+        }
+    }
+}
